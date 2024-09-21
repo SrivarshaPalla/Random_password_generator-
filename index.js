291b601c9b2a896168d00c85df2b585f600f9886
SrivarshaPalla <155418930+SrivarshaPalla@users.noreply.github.com>
2024-09-21 16:59:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
+"U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
+"t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#",
+"$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let password_1_El = document.getElementById("password_1_el");
+
+function generate_a_char(){
+    let random_nr = Math.floor(Math.random() * characters.length);
+    return characters[random_nr];
+}
+
+function generate_password(length_of_password){
+    let password = "";
+    for (let i = 0; i < length_of_password; i++) {
+        password += generate_a_char();
+    }
+    password_1_El.textContent = password;
+}
